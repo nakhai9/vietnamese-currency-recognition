a84050ddee1a740f8b677b2e5fac775540a832cd
nguyenanhkhai <kseptember9cs@gmail.com>
2021-12-26 17:21:45 +0700

    Thesis of Computer Science

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# Vietnamese-Currency-Recognition
+# Vietnamese Currency Recognition
+
+## Overview
+
+Nowadays, banknotes are a major instrument of our transactions. For foreign tourists coming to Vietnam, they are often worried because they don’t know clearly how much the banknote is worth compared to their country's currency or to the common currency as USD, which causes difficulties in the transaction process. Therefore, it is very necessary to build an application program to recognize Vietnamese banknotes. The system uses image feature extraction by principal component analysis (PCA) in combination with support vector machine (SVM) algorithms to build a banknote classification model. We build a model using a radial basis function (Radial Basis Function - RBF) and a polynomial function (Poly). We evaluate the results as accuracy and training time and compare two models to choose the best one. Then display the denomination and equivalent banknote value. Experiments show that the Vietnamese currency recognition system using the radial basis function (RBF) gives an accuracy of 64.24%.
+
+## Vietnamese Banknote Images Dataset
+
+### Data Set information:
+
+- This is a Vietnamese banknote images data set. Each image have size of 28x28 pixel. Image format is used RGB.
+- The data set contains 6 classes of 150-156 KVinstances each, where refer to a type of Vietnamese currency.
+- Predicted attribute: class of Vietnamese Currency.
+
+### Attribute information:
+     There are 6 classes such as:
+
+     | Ord | Label                               | Denomination |
+     | --- | ----------------------------------- | ------------ |
+     | 1   | Ten thousand Vietnam Dongs          | 10,000       |
+     | 2   | Two thousand Vietnam Dongs          | 20,000       |
+     | 3   | Five thousand Vietnam Dongs         | 50,000       |
+     | 4   | One hundred thousand Vietnam Dongs  | 100,000       |
+     | 5   | Two hundred thousand Vietnam Dongs  | 200,000       |
+     | 6   | Five hundred thousand Vietnam Dongs | 500,000       |
+
+### Example:
+
+![image](/screenshot/datasetExample.png)
+
+### Download or discover dataset: [Discover here.](https://github.com/nguyenanhkhai/Vietnamese-Currency-Recognition.git/dataset/RGB.csv)
+
+## Installation
+
+We required Python > 3.8.x to run 
+
+```sh
+git clone https://github.com/nguyenanhkhai/Vietnamese-Currency-Recognition.git
+
+cd Vietnamese-Currency-Recognition
+
+cd app
+
+python app.py
+```
+Then open [http://localhost:5000](http://localhost:5000) to view it in the browser.
+
+## Development Steps
+
+- Step 1: Collects and makes a dataset.
+- Step 2: Preprocessing data.
+- Step 3: Build a model.
+- Step 4: Prediction.
+
+## Technologies Used
+
+- OpenCV.
+- Principal Components Analysis (PCA).
+- Support Vector Machine (SVM).
+- Micro Web Framework: Flask.
+
+## Author
+
+Student: Nguyen Anh Khai. [Contact with me via email](anhkhainguyen9@gmail.com) or [Phone](0945757051)
+
+Instructor: Master Le Thi Phuong Dung. [Contact with her via email](ltpdung@cit.ctu.edu.vn) 
+
+Copyright 12/2021 Nguyen Anh Khai.
---
+++ app/app.py
@@ -0,0 +1,87 @@
+from flask import Flask, render_template, request
+import os
+import cv2
+import numpy as np
+import pickle
+import xml.etree.ElementTree as ET
+
+app = Flask(__name__)
+
+model = pickle.load(open('model.pkl', 'rb'))
+pca = pickle.load(open('pca.pkl', 'rb'))
+
+app.config['UPLOAD_PATH'] = 'static/upload'
+
+def getExchangeRate(path):
+     exchangeRate = []
+     tree = ET.parse('exchangeRate_Vietcombank.xml')
+     root = tree.getroot()
+     for x in root.findall('Exrate'):
+          # print(x.attrib)
+          # print(type(x.attrib))
+          # print(x.attrib['CurrencyCode'])
+          # transfer = x.attrib['Transfer'].replace(',', '')
+          # print(round(10000 / float(transfer), 4))
+          exchangeRate.append(
+               {
+                    'CurrencyName': x.attrib['CurrencyName'].strip(),
+                    'CurrencyCode': x.attrib['CurrencyCode'].strip(),
+                    'CurrencyTransfer': float(x.attrib['Transfer'].replace(',', '').strip())
+               }
+          )
+     return exchangeRate
+
+def getCurrencyTransfer(code):
+     for item in getExchangeRate('exchangeRate_Vietcombank.xml'):
+          if item['CurrencyCode'] == code:
+               return item['CurrencyTransfer'] 
+
+def getCurrencyById(id):
+     labels = [
+          {  "id": 10, "value": "Ten Thousand Vietnam Dongs", 'denomination': 10000},
+          {  "id": 20, "value": "Two Thousand Vietnam Dongs", 'denomination': 20000}, 
+          {  "id": 50, "value": "Five Thousand Vietnam Dongs", 'denomination': 50000 }, 
+          {  "id": 100, "value": "One Hundred Thousand VND", 'denomination': 100000}, 
+          {  "id": 200, "value": "Two Hundred Thousand VND", 'denomination': 200000}, 
+          {  "id": 500, "value": "Five Hundred Thousand VND", 'denomination': 500000},
+     ]
+     for i in labels:
+          if i['id'] == id:
+               return i['value'], i['denomination']
+
+@app.route('/')
+def homePage():
+     return render_template('index.html', title="Thesis", currencies = getExchangeRate('exchangeRate_Vietcombank.xml'))
+
+@app.route('/detection', methods=['POST'])
+def detect():
+     image = request.files['image']
+     image.save(os.path.join(app.config['UPLOAD_PATH'], image.filename))
+
+     IMAGE_SIZE = (28, 28)
+
+     hh = app.config['UPLOAD_PATH']+'/' + image.filename
+
+     image = cv2.imread(app.config['UPLOAD_PATH']+'/' + image.filename)
+     image = cv2.resize(image, IMAGE_SIZE)
+     Image = image.reshape(1, -1)
+
+     img = Image / 255.0
+     principalComponentAnalysis = pca.transform(img)
+     result = model.predict(principalComponentAnalysis)
+
+     # convert2 = [option for option in request.form.values()]
+     convert2 = request.form.get('convert2').strip()
+
+
+     return render_template('detection.html',
+                              title = "Thesis",
+                              label = getCurrencyById(result[0])[0],
+                              transfer = round(getCurrencyById(result[0])[1] / (getCurrencyTransfer(convert2)), 3),
+                              unit = convert2,
+src=hh,
+                              denomination = getCurrencyById(result[0])[1]
+                         )
+
+if __name__ == '__main__':
+    app.run(debug= True);
